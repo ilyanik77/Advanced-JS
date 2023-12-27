c34ee3c102fa80f154fa99475c03190b1ce59b37
ilya <ilyakhlamov77@gmail.com>
2023-12-27 14:36:41 +0300

    add 1

--- HW2/1.js
+++ HW2/1.js
@@ -18,3 +18,76 @@
 библиотеке и возвращать true или false в зависимости от того, есть ли такая 
 книга в списке или нет.
 */
+
+class Library {
+  	#books = [];
+
+  	constructor(books) {
+    	this.#books = books;
+  	}
+
+  	uniquenessListBooks() {
+		const listBooks = new Set(this.#books);
+		try {
+			if(this.#books.length !== listBooks.size) {
+				throw new Error("В списке есть повторяющиеся книги");
+			}
+			return console.log("Ваш список уникален");
+			
+		} catch (error) {
+			console.log(error.message);
+		}
+	}
+
+  	getListBooks() {
+    	return console.log(this.#books.join(",\n"));
+  	}
+
+  	addBookToList(title) {
+    	try {	
+      		if (this.#books.includes(title)) {
+        		throw new Error(`Такая книга уже есть в библиотеке.`);
+      		}
+      		this.#books.push(title);
+    	} catch (error) {
+      		console.log(error.message);
+    	}
+  	}
+
+  	removeBookFromList(title) {
+    const index = this.#books.indexOf(title);
+    	try {
+    	   	if (index === -1) {
+    	     	throw new Error(`Такой книги нет в библиотеке.`);
+    	   	}
+    	   	this.#books.splice(index, 1);
+    	} catch (error) {
+       		console.log(error.message);
+    	}
+  	}
+}
+
+const listBooks = new Library([
+	"Война и мир",
+	//"Нос",
+	"Отцы и дети",
+	"Отрочество",
+	"Мертвые души",
+	"Нос",
+	"Идиот",
+]);
+
+// выводим список
+listBooks.getListBooks();
+// проверяем на уникальность
+listBooks.uniquenessListBooks();
+// удаляем книгу из списка
+listBooks.removeBookFromList("Нос");
+listBooks.getListBooks("Нос");
+// проверяем ошибку
+listBooks.removeBookFromList("Нос");
+// добавляем книгу
+listBooks.addBookToList("Нос");
+listBooks.getListBooks();
+// проверяем ошибку
+listBooks.addBookToList("Нос");
