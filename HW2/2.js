c296e86330ad2264e3c945cf3b4e99acb36cb5f4
ilya <ilyakhlamov77@gmail.com>
2023-12-28 15:49:56 +0300

    add 2

--- HW2/2.js
+++ HW2/2.js
@@ -26,38 +26,87 @@
 */
 
 const initialData = [
-  {
-    id: Date.now(),
-    product: "Apple iPhone 13",
-    reviews: [
-      {
+    {
         id: Date.now(),
-        text: "Отличный телефон! Батарея держится долго.",
-      },
-      {
+        product: "Apple iPhone 13",
+        reviews: [
+            {
+                id: Date.now(),
+                text: "Отличный телефон! Батарея держится долго.",
+            },
+            {
+                id: Date.now(),
+                text: "Камера супер, фото выглядят просто потрясающе.",
+            },
+        ],
+    },
+    {
         id: Date.now(),
-        text: "Камера супер, фото выглядят просто потрясающе.",
-      },
-    ],
-  },
-  {
-    id: Date.now(),
-    product: "Samsung Galaxy Z Fold 3",
-    reviews: [
-      {
+        product: "Samsung Galaxy Z Fold 3",
+        reviews: [
+            {
+                id: Date.now(),
+                text: "Интересный дизайн, но дорогой.",
+            },
+        ],
+    },
+    {
         id: Date.now(),
-        text: "Интересный дизайн, но дорогой.",
-      },
-    ],
-  },
-  {
-    id: Date.now(),
-    product: "Sony PlayStation 5",
-    reviews: [
-      {
-        id: Date.now(),
-        text: "Люблю играть на PS5, графика на высоте.",
-      },
-    ],
-  },
+        product: "Sony PlayStation 5",
+        reviews: [
+            {
+                id: Date.now(),
+                text: "Люблю играть на PS5, графика на высоте.",
+            },
+        ],
+    },
 ];
+
+
+
+initialData.forEach(element => {
+
+    let idProduct = 0;
+    
+    const container = document.createElement("div");
+    container.id = "container";
+    document.body.appendChild(container);
+
+    // Описание продукта
+    const product = document.createElement("div");
+    product.id = "product";
+    container.appendChild(product);
+    product.textContent += element.product;
+    
+    // Отзывы
+    const reviews = document.createElement("div");
+    reviews.id = "reviews";
+    container.appendChild(reviews);
+    element.reviews.forEach(element => {
+        idProduct = element.id;
+        reviews.innerHTML += `<li>${element.text}</li>`;
+    })
+
+    // Поле ввода отзыва
+    const inputText = document.createElement("div");
+    inputText.id = "inputText";
+    container.appendChild(inputText);
+    const message = document.createElement("textarea");
+    inputText.appendChild(message);
+    const btn = document.createElement("button");
+    btn.textContent = "Отправить";
+    btn.addEventListener("click", () => {
+        if (message.value.length >= 50 && message.value.length <= 500) {
+            reviews.innerHTML += `<li>${message.value}</li>`;
+            message.value = " ";
+            errorMessage.textContent = " ";
+        } else {
+            errorMessage.textContent = "Комментарий должен быть длинной не менее 50 и не более 500 символов";
+        }
+    });
+    inputText.appendChild(btn);
+    const errorMessage = document.createElement("p");
+    errorMessage.style.color = "RED";
+    inputText.appendChild(errorMessage);
+
+})
